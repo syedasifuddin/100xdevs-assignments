264cede2893e9ada32a38b0bc69228e916349bf7
Syed <aftabsyedasif@gmail.com>
2024-02-08 23:58:43 +0530

    Fix tsconfig.json build error

---
+++ week-3/02-jwt/test.ts
@@ -0,0 +1,2 @@
+// Adding an empty TypeScript file to avoid the error: tsconfig.json: Build:No inputs were found in config file
+// https://stackoverflow.com/questions/41211566/tsconfig-json-buildno-inputs-were-found-in-config-file
